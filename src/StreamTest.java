0c63bc4d16b0a6de2ddd0e1bb9f050d3812e05c1
mtnsaha <58302068+mtnsaha@users.noreply.github.com>
2023-03-30 23:59:52 +0530

    fourth commit

---
+++ src/StreamTest.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamTest {
+    public static void main(String[] args) {
+        //List<String> list = Arrays.asList("kolkata","hyderabad","Mumbai","Delhi");
+
+        //System.out.println(list.stream().collect(Collectors.toList()));
+        //System.out.println(list);
+        System.out.println("commit complete");
+    }
+}
